6390e6382f9059cacec3188666f938fd519d6e05
techie <techie@techie-ThinkPad-13-2nd-Gen>
2019-02-28 16:26:24 +0530

    First Commit

---
+++ src/main/java/com/tb/java/App.java
@@ -0,0 +1,19 @@
+package com.tb.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ApplicationContext ctx = SpringApplication.run(App.class, args);
+    	System.out.println("Hello world !");
+    }
+}
